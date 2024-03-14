78976f50005025795d45224553d5c5e69c8753e2
GuilhermeBelo2904 <116506659+GuilhermeBelo2904@users.noreply.github.com>
2024-03-14 16:29:29 +0000

    workspace setup

---
+++ Modulo 1/ex3/a.py
@@ -0,0 +1,13 @@
+import math
+import matplotlib
+
+class Symbol:
+  def __init__(self, value, info, entropy, occ):
+    self.value = value  
+    self.info = info
+    self.entropy = entropy
+    self.occ = occ
+    
+    
+    
+    
