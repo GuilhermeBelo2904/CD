624846b763bd84fef72af4bde736fb0fbfecfe93
GuilhermeBelo2904 <116506659+GuilhermeBelo2904@users.noreply.github.com>
2024-03-21 16:11:10 +0000

    3.a) and 3.b)1 done

--- Modulo1/ex2/symbol_frequency.py
+++ Modulo1/ex2/symbol_frequency.py
@@ -1,21 +1,21 @@
 from collections import Counter
 
+class Symbol:
+    def __init__(self, symbol, frequency):
+        self.symbol = symbol
+        self.frequency = frequency
 
-def symbols_with_higher_frequency(filename, percentage_threshold):
-    with open(filename, 'r') as file:
+    def __str__(self):
+        return f"Symbol '{self.symbol}' has frequency {self.frequency}."
+
+def symbols_with_higher_frequency(filename, percentage_threshold = 0):
+    with open(filename, 'rb') as file:
         content = file.read()
 
     symbol_count = Counter(content)
     total_symbols = sum(symbol_count.values())
     percentage_count = total_symbols * (percentage_threshold / 100)
-    symbols_above_percentage = {symbol: count for symbol, count in symbol_count.items() if count > percentage_count}
+    
+    symbols_above_percentage = [Symbol(symbol, count/total_symbols) for symbol, count in symbol_count.items() if count > percentage_count]
     return symbols_above_percentage
 
-
-# test:
-filename = 'text.txt'
-percentage = 20  # 5%
-symbols = symbols_with_higher_frequency(filename, percentage)
-
-for symbol, count in symbols.items():
-    print(f"Symbol '{symbol}' has frequency {count} (above {percentage}%).")
